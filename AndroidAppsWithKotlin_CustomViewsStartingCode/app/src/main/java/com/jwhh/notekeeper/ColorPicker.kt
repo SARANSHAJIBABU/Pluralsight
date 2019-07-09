8c7bea2bbd52a6a7567d438da1408dbfdf0e3532
Saran <ss43285@imcap.ap.ssmb.com>
2019-07-09 23:25:08 +0800

    Custom Views | Draw custom tickmarks

--- AndroidAppsWithKotlin_CustomViewsStartingCode/app/src/main/java/com/jwhh/notekeeper/ColorPicker.kt
+++ AndroidAppsWithKotlin_CustomViewsStartingCode/app/src/main/java/com/jwhh/notekeeper/ColorPicker.kt
@@ -1,7 +1,9 @@
 package com.jwhh.notekeeper
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
 import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.widget.SeekBar
@@ -23,5 +25,35 @@ class ColorPicker @JvmOverloads constructor(
         splitTrack = false
         progressBackgroundTintList = ContextCompat.getColorStateList(context,android.R.color.transparent)
         progressTintList = ContextCompat.getColorStateList(context,android.R.color.transparent)
+        thumb = context.getDrawable(R.drawable.ic_arrow_drop_down_black_24dp)
+        setPadding(paddingLeft,paddingTop,paddingRight,paddingBottom+40)
+        //setBackgroundColor(resources.getColor(android.R.color.black))
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        drawTicks(canvas)
+    }
+
+    fun drawTicks(canvas:Canvas?){
+        canvas?.let {
+            val w = 48
+            val h = 48
+            val halfWidth = if(w==0)1f else w/2f
+            val halfHeight = if(h==0)1f else h/2f
+            val spacing = (width - paddingLeft - paddingRight)/(colorsList.size-1).toFloat()
+
+            val saveCount = canvas.save()
+
+            canvas.translate(paddingLeft.toFloat(),(height/2).toFloat() + 40f)
+
+            for(i in 0 until colorsList.size){
+                val paint = Paint()
+                paint.color = colorsList[i]
+                canvas.drawRect(-halfWidth,-halfHeight,halfWidth,halfHeight,paint)
+                canvas.translate(spacing,0f)
+            }
+            canvas.restoreToCount(saveCount)
+        }
     }
 }
