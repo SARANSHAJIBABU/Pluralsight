2555a98fbcacce56c3a27e27255a0a8fe15a047a
Saran <ss43285@imcap.ap.ssmb.com>
2019-07-21 12:15:56 +0800

    Step 1 | Added RecyclerView

--- NoteKeeper/app/src/main/java/com/jwhh/notekeeper/NoteListActivity.kt
+++ NoteKeeper/app/src/main/java/com/jwhh/notekeeper/NoteListActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
+import com.jwhh.notekeeper.R.id.listNotes
 
 import kotlinx.android.synthetic.main.activity_note_list.*
 import kotlinx.android.synthetic.main.content_note_list.*
@@ -23,22 +24,10 @@ class NoteListActivity : AppCompatActivity() {
         fab.setOnClickListener {
             startActivity(Intent(this, NoteActivity::class.java))
         }
-
-        listNotes.adapter = ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,
-                DataManager.notes)
-
-        listNotes.setOnItemClickListener{parent, view, position, id ->
-            val activityIntent = Intent(this, NoteActivity::class.java)
-            activityIntent.putExtra(NOTE_POSITION, position)
-            startActivity(activityIntent)
-        }
-
     }
 
     override fun onResume() {
         super.onResume()
-        (listNotes.adapter as ArrayAdapter<NoteInfo>).notifyDataSetChanged()
     }
 }
 
