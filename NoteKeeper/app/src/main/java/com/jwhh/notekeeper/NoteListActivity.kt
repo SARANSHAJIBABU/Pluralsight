e8500f4eb8eb37d70a5a237438a75a7e810febd2
Saran <ss43285@imcap.ap.ssmb.com>
2019-07-22 08:42:03 +0800

    Step 3 | Added linearlayoutmanager to recyclerview

--- NoteKeeper/app/src/main/java/com/jwhh/notekeeper/NoteListActivity.kt
+++ NoteKeeper/app/src/main/java/com/jwhh/notekeeper/NoteListActivity.kt
@@ -3,9 +3,11 @@ package com.jwhh.notekeeper
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.ArrayAdapter
 
 import kotlinx.android.synthetic.main.activity_note_list.*
+import kotlinx.android.synthetic.main.content_note_list.*
 
 class NoteListActivity : AppCompatActivity() {
 
@@ -22,6 +24,8 @@ class NoteListActivity : AppCompatActivity() {
         fab.setOnClickListener {
             startActivity(Intent(this, NoteActivity::class.java))
         }
+
+        listItems.layoutManager = LinearLayoutManager(this)
     }
 
     override fun onResume() {
