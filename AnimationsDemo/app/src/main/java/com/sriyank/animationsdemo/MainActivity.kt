1cf4db924a7392b4153ad8b1a39f7fadf0ee475a
Saran <ss43285@imcap.ap.ssmb.com>
2019-08-03 13:19:26 +0800

    Added rotate property animation

--- AnimationsDemo/app/src/main/java/com/sriyank/animationsdemo/MainActivity.kt
+++ AnimationsDemo/app/src/main/java/com/sriyank/animationsdemo/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.sriyank.animationsdemo
 
 import android.animation.AnimatorInflater
-import android.animation.ObjectAnimator
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.view.animation.AlphaAnimation
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,9 +14,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClickAlpha(view: View){
-      //  Toast.makeText(this,"alpha",Toast.LENGTH_SHORT).show()
         val alphaAnimation = AnimatorInflater.loadAnimator(this,R.animator.alpha)
         alphaAnimation?.apply {
+            setTarget(wheel)
+            start()
+        }
+    }
+
+    fun onClickRotate(view: View){
+        val rotateAnimation = AnimatorInflater.loadAnimator(this,R.animator.rotate)
+        rotateAnimation?.apply {
             setTarget(wheel)
             start()
         }
