2d24982edf38da2b41c9a794ec3e1ddfa5bb3f56
Saran <ss43285@imcap.ap.ssmb.com>
2019-08-04 14:19:48 +0800

    Added PropertyValuesHolder for parallel animations on a view

--- AnimationsDemo/app/src/main/java/com/sriyank/animationsdemo/MainActivity.kt
+++ AnimationsDemo/app/src/main/java/com/sriyank/animationsdemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.animation.*
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.ViewPropertyAnimator
 import android.view.animation.BounceInterpolator
 import android.view.animation.OvershootInterpolator
 import android.widget.Toast
@@ -16,18 +17,18 @@ class MainActivity : AppCompatActivity() {
     var translateAnimation:Animator? = null*/
 
 
-    var alphaAnimation:ObjectAnimator? = null
-    var rotateAnimation:ObjectAnimator? = null
-    var scaleAnimation:ObjectAnimator? = null
-    var translateAnimation:ObjectAnimator? = null
-    val mListener = AnimatorListener()
+    var alphaAnimation: ObjectAnimator? = null
+    var rotateAnimation: ObjectAnimator? = null
+    var scaleAnimation: ObjectAnimator? = null
+    var translateAnimation: ObjectAnimator? = null
+    val mListener = Animator.AnimatorListener()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun onClickAlpha(view: View){
+    fun onClickAlpha(view: View) {
 /*        alphaAnimation = AnimatorInflater.loadAnimator(this,R.animator.alpha)
         alphaAnimation?.apply {
             setTarget(wheel)
@@ -36,7 +37,7 @@ class MainActivity : AppCompatActivity() {
 
         }*/
 
-        alphaAnimation = ObjectAnimator.ofFloat(wheel,"alpha",1.0f,0.0f)
+        alphaAnimation = ObjectAnimator.ofFloat(wheel, "alpha", 1.0f, 0.0f)
         alphaAnimation?.apply {
             duration = 1000
             repeatCount = 1
@@ -46,14 +47,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun onClickRotate(view: View){
+    fun onClickRotate(view: View) {
 /*        rotateAnimation = AnimatorInflater.loadAnimator(this,R.animator.rotate)
         rotateAnimation?.apply {
             setTarget(wheel)
             addListener(mListener)
             start()
         }*/
-        rotateAnimation = ObjectAnimator.ofFloat(wheel,"rotation",0.0f,-180.0f)
+        rotateAnimation = ObjectAnimator.ofFloat(wheel, "rotation", 0.0f, -180.0f)
         rotateAnimation?.apply {
             duration = 1000
             repeatCount = 1
@@ -63,14 +64,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun onClickScale(view: View){
- /*       scaleAnimation = AnimatorInflater.loadAnimator(this,R.animator.scale)
-        scaleAnimation?.apply {
-            setTarget(wheel)
-            addListener(mListener)
-            start()
-        }*/
-        scaleAnimation = ObjectAnimator.ofFloat(wheel,"scaleX",1.0f,1.5f)
+    fun onClickScale(view: View) {
+        /*       scaleAnimation = AnimatorInflater.loadAnimator(this,R.animator.scale)
+               scaleAnimation?.apply {
+                   setTarget(wheel)
+                   addListener(mListener)
+                   start()
+               }*/
+        scaleAnimation = ObjectAnimator.ofFloat(wheel, "scaleX", 1.0f, 1.5f)
         scaleAnimation?.apply {
             duration = 1000
             repeatMode = ValueAnimator.REVERSE
@@ -80,7 +81,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun onClickTranslate(view: View){
+    fun onClickTranslate(view: View) {
 /*        translateAnimation = AnimatorInflater.loadAnimator(this,R.animator.translate)
         translateAnimation?.apply {
             setTarget(wheel)
@@ -88,7 +89,7 @@ class MainActivity : AppCompatActivity() {
             start()
         }*/
 
-        translateAnimation = ObjectAnimator.ofFloat(wheel,"translationX",0.0f,200.0f)
+        translateAnimation = ObjectAnimator.ofFloat(wheel, "translationX", 0.0f, 200.0f)
         translateAnimation?.apply {
             duration = 1000
             repeatCount = 1
@@ -98,23 +99,23 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun onClickSetXml(view: View){
-        val animator = AnimatorInflater.loadAnimator(this,R.animator.set)
+    fun onClickSetXml(view: View) {
+        val animator = AnimatorInflater.loadAnimator(this, R.animator.set)
         animator?.apply {
             setTarget(wheel)
             start()
         }
     }
 
-    fun onClickSetCode(view: View){
+    fun onClickSetCode(view: View) {
         val rootSet = AnimatorSet()
-        val rotationY = ObjectAnimator.ofFloat(wheel,"rotationY",0.0f,360.0f);
+        val rotationY = ObjectAnimator.ofFloat(wheel, "rotationY", 0.0f, 360.0f);
         rotationY.duration = 1000
         val childSet = AnimatorSet()
-        val scaleX = ObjectAnimator.ofFloat(wheel,"scaleX",1.0f,1.5f)
+        val scaleX = ObjectAnimator.ofFloat(wheel, "scaleX", 1.0f, 1.5f)
         scaleX.duration = 1000
         scaleX.interpolator = BounceInterpolator()
-        val scaleY = ObjectAnimator.ofFloat(wheel,"scaleY",1.0f,1.5f)
+        val scaleY = ObjectAnimator.ofFloat(wheel, "scaleY", 1.0f, 1.5f)
         scaleY.duration = 1000
         scaleY.interpolator = BounceInterpolator()
 /*        childSet.playTogether(scaleX,scaleY)
@@ -125,7 +126,13 @@ class MainActivity : AppCompatActivity() {
         rootSet.start()
     }
 
-    fun onClickViewPropertyAnimation(view:View){
+    /*
+        Animation runs in parallel. Dont require start values
+        Since ViewPropertyAnimator doesnt use ObjectAnimator, animations cant be controlled
+        Suitable for simple animations
+        Efficient than objectanimator
+    * */
+    fun onClickViewPropertyAnimation(view: View) {
         val animator = wheel.animate()
         animator.apply {
             duration = 1000
@@ -138,40 +145,102 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    inner class AnimatorListener:Animator.AnimatorListener{
+    /*
+        Animation runs in parallel. Dont require start values
+        Since PropertyValuesHolder uses ObjectAnimator, animations can be controlled by using animatorlistener
+    * */
+    fun onClickViewPropertyHolder(view: View) {
+        val rotationX = PropertyValuesHolder.ofFloat("rotationX", 360.0f)
+        val scaleX = PropertyValuesHolder.ofFloat("scaleX", 1.5f)
+        val scaleY = PropertyValuesHolder.ofFloat("scaleY", 1.5f)
+
+        val animator = ObjectAnimator.ofPropertyValuesHolder(wheel, rotationX, scaleX, scaleY)
+        animator.apply {
+            animator.duration = 1000
+            animator.interpolator = BounceInterpolator()
+            start()
+        }
+    }
+
+
+    inner class AnimatorListener : Animator.AnimatorListener {
         override fun onAnimationRepeat(animation: Animator?) {
-            when(animation){
-                translateAnimation->Toast.makeText(this@MainActivity,"translateAnimation onRepeat",Toast.LENGTH_SHORT).show()
-                rotateAnimation->Toast.makeText(this@MainActivity,"rotateAnimation onRepeat",Toast.LENGTH_SHORT).show()
-                scaleAnimation->Toast.makeText(this@MainActivity,"scaleAnimation onRepeat",Toast.LENGTH_SHORT).show()
-                alphaAnimation->Toast.makeText(this@MainActivity,"alphaAnimation onRepeat",Toast.LENGTH_SHORT).show()
+            when (animation) {
+                translateAnimation -> Toast.makeText(
+                    this@MainActivity,
+                    "translateAnimation onRepeat",
+                    Toast.LENGTH_SHORT
+                ).show()
+                rotateAnimation -> Toast.makeText(
+                    this@MainActivity,
+                    "rotateAnimation onRepeat",
+                    Toast.LENGTH_SHORT
+                ).show()
+                scaleAnimation -> Toast.makeText(
+                    this@MainActivity,
+                    "scaleAnimation onRepeat",
+                    Toast.LENGTH_SHORT
+                ).show()
+                alphaAnimation -> Toast.makeText(
+                    this@MainActivity,
+                    "alphaAnimation onRepeat",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
 
         override fun onAnimationEnd(animation: Animator?) {
-            when(animation){
-                translateAnimation->Toast.makeText(this@MainActivity,"translateAnimation onEnd",Toast.LENGTH_SHORT).show()
-                rotateAnimation->Toast.makeText(this@MainActivity,"rotateAnimation onEnd",Toast.LENGTH_SHORT).show()
-                scaleAnimation->Toast.makeText(this@MainActivity,"scaleAnimation onEnd",Toast.LENGTH_SHORT).show()
-                alphaAnimation->Toast.makeText(this@MainActivity,"alphaAnimation onEnd",Toast.LENGTH_SHORT).show()
+            when (animation) {
+                translateAnimation -> Toast.makeText(
+                    this@MainActivity,
+                    "translateAnimation onEnd",
+                    Toast.LENGTH_SHORT
+                ).show()
+                rotateAnimation -> Toast.makeText(this@MainActivity, "rotateAnimation onEnd", Toast.LENGTH_SHORT).show()
+                scaleAnimation -> Toast.makeText(this@MainActivity, "scaleAnimation onEnd", Toast.LENGTH_SHORT).show()
+                alphaAnimation -> Toast.makeText(this@MainActivity, "alphaAnimation onEnd", Toast.LENGTH_SHORT).show()
             }
         }
 
         override fun onAnimationCancel(animation: Animator?) {
-            when(animation){
-                translateAnimation->Toast.makeText(this@MainActivity,"translateAnimation onCancel",Toast.LENGTH_SHORT).show()
-                rotateAnimation->Toast.makeText(this@MainActivity,"rotateAnimation onCancel",Toast.LENGTH_SHORT).show()
-                scaleAnimation->Toast.makeText(this@MainActivity,"scaleAnimation onCancel",Toast.LENGTH_SHORT).show()
-                alphaAnimation->Toast.makeText(this@MainActivity,"alphaAnimation onCancel",Toast.LENGTH_SHORT).show()
+            when (animation) {
+                translateAnimation -> Toast.makeText(
+                    this@MainActivity,
+                    "translateAnimation onCancel",
+                    Toast.LENGTH_SHORT
+                ).show()
+                rotateAnimation -> Toast.makeText(
+                    this@MainActivity,
+                    "rotateAnimation onCancel",
+                    Toast.LENGTH_SHORT
+                ).show()
+                scaleAnimation -> Toast.makeText(
+                    this@MainActivity,
+                    "scaleAnimation onCancel",
+                    Toast.LENGTH_SHORT
+                ).show()
+                alphaAnimation -> Toast.makeText(
+                    this@MainActivity,
+                    "alphaAnimation onCancel",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
 
         override fun onAnimationStart(animation: Animator?) {
-            when(animation){
-                translateAnimation->Toast.makeText(this@MainActivity,"translateAnimation onStart",Toast.LENGTH_SHORT).show()
-                rotateAnimation->Toast.makeText(this@MainActivity,"rotateAnimation onStart",Toast.LENGTH_SHORT).show()
-                scaleAnimation->Toast.makeText(this@MainActivity,"scaleAnimation onStart",Toast.LENGTH_SHORT).show()
-                alphaAnimation->Toast.makeText(this@MainActivity,"alphaAnimation onStart",Toast.LENGTH_SHORT).show()
+            when (animation) {
+                translateAnimation -> Toast.makeText(
+                    this@MainActivity,
+                    "translateAnimation onStart",
+                    Toast.LENGTH_SHORT
+                ).show()
+                rotateAnimation -> Toast.makeText(
+                    this@MainActivity,
+                    "rotateAnimation onStart",
+                    Toast.LENGTH_SHORT
+                ).show()
+                scaleAnimation -> Toast.makeText(this@MainActivity, "scaleAnimation onStart", Toast.LENGTH_SHORT).show()
+                alphaAnimation -> Toast.makeText(this@MainActivity, "alphaAnimation onStart", Toast.LENGTH_SHORT).show()
             }
         }
 
